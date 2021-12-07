ceee8593e124c9eea3bf717070a768578d51a7fb
anil8040 <81.anil.kumar@gmail.com>
2021-12-07 05:25:19 -0500

    Assignment 1

--- src/shared/dishes.js
+++ src/shared/dishes.js
@@ -8,6 +8,44 @@ export const DISHES = [
     price: "4.99",
     description:
       "A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.",
+    comments: [
+      {
+        id: 0,
+        rating: 5,
+        comment: "Imagine all the eatables, living in conFusion!",
+        author: "John Lemn",
+        date: "2012-10-16T17:57:28.556094Z",
+      },
+      {
+        id: 1,
+        rating: 4,
+        comment:
+          "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
+        author: "Paul McVites",
+        date: "2014-09-05T17:57:28.556094Z",
+      },
+      {
+        id: 2,
+        rating: 3,
+        comment: "Eat it, just eat it!",
+        author: "Michael Jaik",
+        date: "2015-02-13T17:57:28.556094Z",
+      },
+      {
+        id: 3,
+        rating: 4,
+        comment: "Ultimate, Reaching for the stars!",
+        author: "Ringo Star",
+        date: "2013-12-02T17:57:28.556094Z",
+      },
+      {
+        id: 4,
+        rating: 2,
+        comment: "It's your birthday, we're gonna party!",
+        author: "Johen Delmon",
+        date: "˙011-12-02T17:57:28.556094Z",
+      },
+    ],
   },
   {
     id: 1,
@@ -18,6 +56,44 @@ export const DISHES = [
     price: "1.99",
     description:
       "Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce",
+    comments: [
+      {
+        id: 0,
+        rating: 5,
+        comment: "Imagine all the eatables, living in conFusion!",
+        author: "Jhn Lemon",
+        date: "2012-10-16T17:57:28.556094Z",
+      },
+      {
+        id: 1,
+        rating: 4,
+        comment:
+          "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
+        author: "Pal McVites",
+        date: "2014-09-05T17:57:28.556094Z",
+      },
+      {
+        id: 2,
+        rating: 3,
+        comment: "Eat it, just eat it!",
+        author: "Mihael Jaikishan",
+        date: "2015-02-13T17:57:28.556094Z",
+      },
+      {
+        id: 3,
+        rating: 4,
+        comment: "Ultimate, Reaching for the stars!",
+        author: "ngo Starry",
+        date: "2013-12-02T17:57:28.556094Z",
+      },
+      {
+        id: 4,
+        rating: 2,
+        comment: "It's your birthday, we're gonna party!",
+        author: "Centimani",
+        date: "2011-12-02T17:57:28.556094Z",
+      },
+    ],
   },
   {
     id: 2,
@@ -28,6 +104,44 @@ export const DISHES = [
     price: "1.99",
     description:
       "A quintessential ConFusion experience, is it a vada or is it a donut?",
+    comments: [
+      {
+        id: 0,
+        rating: 5,
+        comment: "Imagine all the eatables, living in conFusion!",
+        author: "John Lemon",
+        date: "2012-10-16T17:57:28.556094Z",
+      },
+      {
+        id: 1,
+        rating: 4,
+        comment:
+          "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
+        author: "Pal McVites",
+        date: "2014-09-05T17:57:28.556094Z",
+      },
+      {
+        id: 2,
+        rating: 3,
+        comment: "Eat it, just eat it!",
+        author: "Michel Jaikishan",
+        date: "2015-02-13T17:57:28.556094Z",
+      },
+      {
+        id: 3,
+        rating: 4,
+        comment: "Ultimate, Reaching for the stars!",
+        author: "Rigo Starry",
+        date: "2013-12-02T17:57:28.556094Z",
+      },
+      {
+        id: 4,
+        rating: 2,
+        comment: "It's your birthday, we're gonna party!",
+        author: "Reasgaseent",
+        date: "2011-12-02T17:57:28.556094Z",
+      },
+    ],
   },
   {
     id: 3,
@@ -38,5 +152,43 @@ export const DISHES = [
     price: "2.99",
     description:
       "A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms",
+    comments: [
+      {
+        id: 0,
+        rating: 5,
+        comment: "Imagine all the eatables, living in conFusion!",
+        author: "John Lem",
+        date: "2012-10-16T17:57:28.556094Z",
+      },
+      {
+        id: 1,
+        rating: 4,
+        comment:
+          "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
+        author: "Paul Mcs",
+        date: "2014-09-05T17:57:28.556094Z",
+      },
+      {
+        id: 2,
+        rating: 3,
+        comment: "Eat it, just eat it!",
+        author: "Michael J",
+        date: "2015-02-13T17:57:28.556094Z",
+      },
+      {
+        id: 3,
+        rating: 4,
+        comment: "Ultimate, Reaching for the stars!",
+        author: "Ringo Starry III",
+        date: "2013-12-02T17:57:28.556094Z",
+      },
+      {
+        id: 4,
+        rating: 2,
+        comment: "It's your birthday, we're gonna party!",
+        author: "2Centurion",
+        date: "2011-12-02T17:57:28.556094Z",
+      },
+    ],
   },
 ];
