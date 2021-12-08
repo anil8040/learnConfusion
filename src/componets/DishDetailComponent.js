02d80c02c15c8c22860cb56c26fe3d68e09f6032
anil8040 <81.anil.kumar@gmail.com>
2021-12-08 07:01:36 -0500

    Presentational and container components

--- src/componets/DishDetailComponent.js
+++ src/componets/DishDetailComponent.js
@@ -15,66 +15,72 @@ class DishDetail extends Component {
   }
 
   render() {
-    let selectedDish = this.props.selectedDish;
-    let comments = selectedDish.comments.map((comment) => {
+    let selectedDish = this.props.dish;
+    if (selectedDish) {
+      let comments = selectedDish.comments.map((comment) => {
+        return (
+          <ul key={comment.id} className="list-unstyled">
+            <li> - Rating : {comment.rating}</li>
+            <li> - Comment : {comment.comment}</li>
+            <li>
+              {" "}
+              - Author : {comment.author} , #{" "}
+              {new Intl.DateTimeFormat("en-US", {
+                year: "numeric",
+                month: "short",
+                day: "2-digit",
+              }).format(new Date(Date.parse(comment.date)))}
+            </li>
+          </ul>
+        );
+      });
+
+      //This is one way
+      // let comments = this.props.selectedDish.comments.map((comment) => {
+      //   return (
+      //     <ul key={comment.id} class="list">
+      //       <li>-- Rating : {comment.rating}</li>
+      //       <li>-- Comment : {comment.comment}</li>
+      //       <li>
+      //         -- Author : {comment.author} , {comment.date}{" "}
+      //       </li>
+      //     </ul>
+
+      //   );
+      // });
+
+      if (!selectedDish) return <div></div>;
       return (
-        <ul key={comment.id} className="list-unstyled">
-          <li>-- Rating : {comment.rating}</li>
-          <li>-- Comment : {comment.comment}</li>
-          <li>
-            -- Author : {comment.author} , # {comment.date}
-          </li>
-          {/* id={comment.id}
-        rating={comment.rating}
-        author={comment.rating}
-        author={comment.auhor} */}
-        </ul>
+        <div className="container">
+          <div className="row">
+            <div className="col-12 col-md-5 m-1">
+              <Card>
+                <CardImg
+                  width="100%"
+                  src={selectedDish.image}
+                  alt={selectedDish.name}
+                />
+                <CardImgOverlay>
+                  <CardTitle>
+                    <b>{selectedDish.name}</b>
+                  </CardTitle>
+                </CardImgOverlay>
+                <CardBody>
+                  <CardTitle>{selectedDish.name}</CardTitle>
+                  <CardText>{selectedDish.description}</CardText>
+                </CardBody>
+              </Card>
+            </div>
+            <div className="col-12 col-md-5 m-1">
+              <b>Comments </b>
+              {comments}
+            </div>
+          </div>
+        </div>
       );
-    });
-
-    //This is one way
-    // let comments = this.props.selectedDish.comments.map((comment) => {
-    //   return (
-    //     <ul key={comment.id} class="list">
-    //       <li>-- Rating : {comment.rating}</li>
-    //       <li>-- Comment : {comment.comment}</li>
-    //       <li>
-    //         -- Author : {comment.author} , {comment.date}{" "}
-    //       </li>
-    //     </ul>
-
-    //   );
-    // });
-
-    if (!selectedDish) return <div></div>;
-    return (
-      <div className="row">
-        <div className="col-12 col-md-5 m-1">
-          <Card>
-            <CardImg
-              width="100%"
-              src={selectedDish.image}
-              alt={selectedDish.name}
-            />
-            <CardImgOverlay>
-              <CardTitle>
-                <b>{selectedDish.name}</b>
-              </CardTitle>
-            </CardImgOverlay>
-            <CardBody>
-              <CardTitle>{selectedDish.name}</CardTitle>
-              <CardText>{selectedDish.description}</CardText>
-            </CardBody>
-          </Card>
-        </div>
-        <div className="col-12 col-md-5 m-1">
-          <Card>
-            <b>Comments </b>
-            {comments}
-          </Card>
-        </div>
-      </div>
-    );
+    } else {
+      return <div></div>;
+    }
   }
 }
 
